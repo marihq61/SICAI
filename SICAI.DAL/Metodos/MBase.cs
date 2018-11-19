d38e98352acb7ce69dc3d5e295427949c29f50ef
Mariana Hernandez Quesada <marihq61@gmail.com>
2018-11-19 12:23:23 -0600

    conexion BD e intento fallido con el CSS :( no lo logro

---
+++ SICAI.DAL/Metodos/MBase.cs
@@ -0,0 +1,23 @@
+﻿using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SICAI.DAL.Metodos
+{
+    public class MBase
+    {
+        public OrmLiteConnectionFactory _conexion;
+        public IDbConnection _db;
+
+        public MBase()
+        {
+            _conexion = new OrmLiteConnectionFactory(Settings.Default.conexion,
+                SqlServerDialect.Provider);
+            _db = _conexion.Open();
+        }
+    }
+}
